9bbb5adcede0883d333c6613c86ea34bafa2aebe
Benjamin Jacobs <contact@ttocsneb.com>
2018-02-03 18:58:40 -0700

    Add Device/Remote Interface

--- src/rcDeviceProtocol.h
+++ src/rcDeviceProtocol.h
@@ -10,6 +10,7 @@
 
 #include "rcSettings.h"
 #include "rcGlobal.h"
+#include "rcDeviceProtocol.h"
 
 #ifndef __RF24_H__
 #error "rcDeviceProtocol Requires the tmrh20 RF24 Library: https://github.com/nRF24/RF24"
@@ -26,41 +27,6 @@
  */
 class DeviceProtocol : RCGlobal {
 public:
-  /**
-   * Save the transmitter id to non-volitile memory.
-   *
-   * Very simple, all that needs to be done, is to save the Id to memory,
-   * so that when the receiver tries to connect, it will know which id to
-   * request to connect with.
-   *
-   * @param id 5 byte char array
-   */
-  typedef void (saveRemoteID)(const uint8_t* id);
-  /**
-   * Load the transmitter id from non-volitile memory.
-   *
-   * Load the transmitter id set from saveRemoteID() into id
-   *
-   * @param id 5 byte array to put remote id in.
-   */
-  typedef void (loadRemoteID)(uint8_t* id);
-  /**
-   * Check if the device was previously connected to the remote.
-   *
-   * This checks the data set from setConnected(), and should be non-volitile
-   *
-   * @return true if connected.
-   */
-  typedef bool (checkConnected)();
-  /**
-   * Set whether the device is connected to the remote.
-   *
-   * @note This should be non-volitile, meaning the value set by this should
-   * remain after a power cycle or reset
-   *
-   * @param connected true if connected, false if not.
-   */
-  typedef void (setConnected)(bool connected);
 
   /**
    * Constructor
@@ -84,29 +50,25 @@ public:
    * does this for you
    *
    * @param settings RCSettings
-   * @param checkConnected checkConnected()
-   * @param loadRemoteID loadRemoteID()
+   * @param functions User-defined functions from #DeviceInterface
    *
    * @return 0
    * @return 1 if reconnected to remote
    * @return -1 if unable to reconnect to remtoe
    */
-  int8_t begin(RCSettings* settings, checkConnected checkConnected,
-               loadRemoteID loadRemoteID);
+  int8_t begin(RCSettings* settings, DeviceInterface* functions);
 
   /**
    * Attempt to pair with a transmitter
    *
    * @note The transmitter you are trying to pair with should also be in pair mode
    *
-   * @param saveRemoteID A function pointer to save the id of the transmitter.
-   *
    * @return 0 if successful
    * @return #RC_ERROR_ALREADY_CONNECTED if already connected to remote
    * @return #RC_ERROR_TIMEOUT if no transmitter was found.
    * @return #RC_ERROR_LOST_CONNECTION if transmitter stopped replying
    */
-  int8_t pair(saveRemoteID saveRemoteID);
+  int8_t pair();
 
   /**
    * Check if the receiver is connected with a transmitter.
@@ -121,9 +83,6 @@ public:
    * @note The transmitter you are trying to connect with should also be in connect mode,
    * as well as paired with this device
    *
-   * @param loadRemoteID loadRemoteID()
-   * @param setConnected setConnected()
-   *
    * @return 0 if successful
    * @return #RC_ERROR_ALREADY_CONNECTED if already connected to remote
    * @return #RC_ERROR_TIMEOUT if no transmitter was found
@@ -132,7 +91,7 @@ public:
    * @return #RC_ERROR_BAD_DATA if the settings are not properly set, or
    * the transmitter sent unexpected data
    */
-  int8_t connect(loadRemoteID loadRemoteID, setConnected setConnected);
+  int8_t connect();
 
   /**
    * Update the communications with the currently connected device
@@ -143,14 +102,12 @@ public:
    * when a standard packet is received.
    * @param telemetry RCSettings.setPayloadSize() size array of telemetry
    * data to send to the transmitter
-   * @param setConnected setConnected()
    *
    * @return 1 if channels were updated
    * @return 0 if nothing happened
    * @return #RC_ERROR_NOT_CONNECTED if not connected
    */
-  int8_t update(uint16_t channels[], uint8_t telemetry[],
-                setConnected setConnected);
+  int8_t update(uint16_t channels[], uint8_t telemetry[]);
 
   /**
    * Get pointer for the current settings
@@ -163,6 +120,7 @@ private:
   const uint8_t* _deviceId;
   uint8_t _remoteId[5];
   bool _isConnected;
+  DeviceInterface* _deviceFunctions;
 
 
   /**
